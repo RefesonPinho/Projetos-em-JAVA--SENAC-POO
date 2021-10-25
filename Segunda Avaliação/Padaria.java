f972d99150344c68e13d6fba7d2561736a5b4cb7
Refeson dos Santos Pinho <89049212+RefesonPinho@users.noreply.github.com>
2021-10-25 17:21:37 -0300

    Add files via upload

---
+++ Segunda Avaliação/Padaria.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class Padaria {
+
+    String id;
+    String nome;
+    String datadeabertura;
+    Endereco endereco;
+    ArrayList <Receita> pratos = new ArrayList<>();
+
+    public Padaria(
+
+        String id,
+        String nome,
+        String datadeabertura,
+        String idendereco,
+        String cep,
+        String rua,
+        int numero,
+        String bairro,
+        String cidade
+       
+
+    
+    ){
+        this.id = id;
+        this.nome = nome;
+        this.datadeabertura = datadeabertura;
+        this.endereco = new Endereco(idendereco, cep, rua, numero, bairro, cidade,this);
+
+    }
+
+    public void adicionarReceita(Receita receita){
+        this.pratos.add(receita);
+    }
+    
+
+
+
+}
+
+    
