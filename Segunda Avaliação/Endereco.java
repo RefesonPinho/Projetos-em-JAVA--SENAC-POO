d25f32ec46d294571738d488c37f943ae94b0fe0
Refeson dos Santos Pinho <89049212+RefesonPinho@users.noreply.github.com>
2021-10-25 17:34:15 -0300

    Add files via upload

---
+++ Segunda Avaliação/Endereco.java
@@ -0,0 +1,32 @@
+public class Endereco {
+    String id;
+    String cep;
+    String rua;
+    int numero;
+    String bairro;
+    String cidade; 
+    Padaria padaria;
+
+    public Endereco(
+
+        String id,
+        String cep,
+        String rua,
+        int numero,
+        String bairro,
+        String cidade, 
+        Padaria padaria
+    ){
+       this.id = id;
+       this.cep = cep;
+       this.rua = rua;
+       this.numero = numero;
+       this.bairro = bairro;
+       this.cidade = cidade;
+       this.padaria = padaria;
+    }
+}  
+
+
+    
+
---
+++ Segunda Avaliação/Receita.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Receita {
+
+    String idreceita;
+    String nomereceita;
+    String etapas;
+    int numeroetapas; 
+    Chef chef;
+    ArrayList <Padaria> padaria = new ArrayList<>();
+
+    public Receita(
+
+        String idreceita,
+        String nomereceita,
+        String etapas,
+        int numeroetapas,
+        String idchefe,
+        String nome,
+        String cpf,
+        String datanascimento
+    ){
+        this.idreceita = idreceita;
+        this.nomereceita = nomereceita;
+        this.etapas = etapas;
+        this.numeroetapas = numeroetapas;
+        this.chef = new Chef(idchefe, nome, cpf, datanascimento);
+    }
+        
+    public void adicionarPadaria(Padaria padaria){
+        this.padaria.add(padaria);
+        
+    }
+}
+
