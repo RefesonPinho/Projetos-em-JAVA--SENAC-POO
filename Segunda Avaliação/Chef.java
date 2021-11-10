58c7e84a2690d8523b27807f6307217e6c9a9f13
Refeson dos Santos Pinho <89049212+RefesonPinho@users.noreply.github.com>
2021-11-09 23:43:47 -0300

    Update Chef.java

--- Segunda Avaliação/Chef.java
+++ Segunda Avaliação/Chef.java
@@ -1,70 +1,30 @@
 import java.util.ArrayList;
-import java.util.Objects;
 
-public class Chef extends Pessoass {
+public class Chef {
     
-    private String idChef;
-    private String especialidade;
-    private ArrayList<Receita> receitas = new  ArrayList <>(); 
+    String idchefe;
+    String nome;
+    String cpf;
+    String datanascimento;
+    ArrayList<Receita> receitas = new  ArrayList <>(); 
 
     public Chef(
+        String idchefe,
         String nome,
         String cpf,
-        String dataNascimento,
-        String idChef,
-        String especialidade
-        
+        String datanascimento
     ){
-        super(nome, cpf, dataNascimento);
-        this.idChef = idChef;
-        this.especialidade = especialidade; 
-    }
 
-    public void setIdChef(String idChef){
-        this.idChef = idChef;
-    }
-
-    public String getIdChef(){
-        return this.idChef;
-    }
-
-    public void setEspecialidade(String especialidade){
-        this.especialidade = especialidade;
-    }
-
-    public String getEspecialidade(){
-        return this.especialidade;
+       this.idchefe = idchefe;
+       this.nome = nome;
+       this.cpf = cpf;
+       this.datanascimento = datanascimento; 
     }
     
-    public void setReceita( Receita receita){
+    public void adicionarReceita( Receita receita){
         this.receitas.add(receita);      
-    }
-    
-    public ArrayList<Receita> getReceita(){
-        return this.receitas;
-    }
-
-    @Override
-    public boolean equals(Object c) {
-        if (c == this)
-            return true;
-        if (!(c instanceof Chef)) {
-            return false;
-        }
-        Chef chef = (Chef) c;
-        return Objects.equals(this.getCPF(), chef.getCPF());
-    }
-
-    @Override
-    public String toString() {
-        return   "Nome :" 
-            + getNome() + " / " + "CPF:"
-            + getCPF() + " / " + "Data de Nascimento:" 
-            + getDataNascimento() + " / " + "Id do Chefe:" 
-            + getIdChef() + " / " + "Especialidade:"
-            + getEspecialidade();
-    }
-
-    
+    } 
 }
+
+
 
