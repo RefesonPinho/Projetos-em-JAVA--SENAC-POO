63ed06b7137b1b24f48505a1a86ff875bc73b5d3
Refeson dos Santos Pinho <89049212+RefesonPinho@users.noreply.github.com>
2021-11-09 23:51:29 -0300

    Update Padaria.java

--- Segunda Avaliação/Padaria.java
+++ Segunda Avaliação/Padaria.java
@@ -1,80 +1,34 @@
 import java.util.ArrayList;
-import java.util.Objects;
 
-public class Padaria extends Estabelecimento {
+public class Padaria {
 
-    private String horarioFuncionamento;
-    private ArrayList <Receita> pratos = new ArrayList<>();
+    String id;
+    String nome;
+    String datadeabertura;
+    Endereco endereco;
+    ArrayList <Receita> pratos = new ArrayList<>();
 
     public Padaria(
-        String idAlvara,
-        String nomeEstabelecimento,
+
+        String id,
+        String nome,
         String datadeabertura,
-        String cidade,
-        String bairro,
+        String idendereco,
         String cep,
         String rua,
         int numero,
-        String horarioFuncionamento
-        
+        String bairro,
+        String cidade  
     ){
-        super(idAlvara, nomeEstabelecimento, datadeabertura, cidade, bairro, cep, rua, numero);
-        this.horarioFuncionamento = horarioFuncionamento;
-    
+        this.id = id;
+        this.nome = nome;
+        this.datadeabertura = datadeabertura;
+        this.endereco = new Endereco(idendereco, cep, rua, numero, bairro, cidade,this);
     }
 
-    public void setHorarioFuncionamento(String horarioFuncionamento){
-        this.horarioFuncionamento = horarioFuncionamento;
-    }
-    
-    public String getHorarioFuncionamento(){
-        return this.horarioFuncionamento;
-    }
-
-    public void setReceita(Receita receita){
+    public void adicionarReceita(Receita receita){
         this.pratos.add(receita);
-        receita.setPadaria(this);
-        
-    }
-    
-    public ArrayList<Receita> getReceita(){
-        return this.pratos;
-    }
-
-    // Fazendo uso do padrão de projeto Decorator 
-    @Override
-    public boolean equals(Object p) {
-        if (p == this)
-            return true;
-        if (!(p instanceof Padaria)) {
-            return false;
-        }
-        Padaria padaria = (Padaria) p;
-        return Objects.equals(this.getIdAlvara(), padaria.getIdAlvara());
-    }
-
-
-    @Override
-    public String toString() {
-        String total="Id do Alvará:" 
-            + getIdAlvara() + " / " + "Nome do Estabelecimento:"
-            + getNomeEstabelecimento() + " / " + "Data de abertura:" 
-            + getDataAbertura() + " / " + "Cidade:" 
-            + getEndereco().getCidade() + " / " + "Bairro:"
-            + getEndereco().getBairro() + " / " + "Cep:"
-            + getEndereco().getCep() + " / " + "Rua:"  
-            + getEndereco().getRua() + " / " + "Número:"
-            + getEndereco().getNumero() + " / " + "Horário de funcionamento:"
-            + getHorarioFuncionamento() + "Quantidade de Receitas:"
-            + this.pratos.size();
-            
-            for(Receita receita : this.pratos){
-                total += "\n   "+ receita.dadosReceita();
-
-            };
-        return total;    
-    }
-    
+    }   
 }
     
 
