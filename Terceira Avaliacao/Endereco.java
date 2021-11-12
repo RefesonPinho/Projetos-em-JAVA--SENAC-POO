cfd19cdb37ff2509d6493e668ce44ad5d4737b84
REFESON DOS SANTOS PINHO <refeson.pinho@alunos.sc.senac.br>
2021-11-11 21:27:39 -0300

    Pequenas Alteracoes

--- Terceira Avaliacao/Endereco.java
+++ Terceira Avaliacao/Endereco.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 // Criando a classe Endereço
 public class Endereco {
     /// Encapsulando os atributos
@@ -73,6 +75,29 @@ public class Endereco {
 
     public Estabelecimento getEstabelecimento(){
         return this.estabelecimento;
+    }
+
+    // Fazendo uso do padrão de projeto Decorator 
+    @Override
+    public boolean equals(Object e) {
+        if (e == this)
+            return true;
+        if (!(e instanceof Endereco)) {
+            return false;
+        }
+        Endereco endereco = (Endereco) e;
+        return Objects.equals(this.getNumero(), endereco.getNumero());
+    }
+
+    
+    @Override
+    public String toString() {
+        return   "Cidade: " 
+            + getCidade() + " / " + "Bairro:"
+            + getBairro() + " / " + "Cep:"
+            + getCep() + " / " + "Rua:" 
+            + getRua() + " / " + "Numero:" 
+            + getNumero();
     }
 }  
 
